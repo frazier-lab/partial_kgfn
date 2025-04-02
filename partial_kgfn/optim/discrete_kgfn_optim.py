d85d6f19f1ffa66299b0d7404e61831d628f8697
Poompol Buathong <104692189+poompol@users.noreply.github.com>
2025-04-02 18:43:10 -0400

    Update discrete_kgfn_optim.py

--- partial_kgfn/optim/discrete_kgfn_optim.py
+++ partial_kgfn/optim/discrete_kgfn_optim.py
@@ -4,22 +4,21 @@
 # LICENSE file in the root directory of this source tree.
 
 r"""
-Method for optimizing discrete knowledge gradient-based acquisition functions for function network
+Methods for optimizing decoupled acquisition functions.
 """
-
 import itertools
 import time
-from typing import Any, List, Optional
+from typing import Any, List, Optional, Tuple
 
 import numpy as np
 import torch
 from botorch.acquisition import AcquisitionFunction
-from botorch.acquisition.fixed_feature import FixedFeatureAcquisitionFunction
 from botorch.logging import logger
-from botorch.optim.optimize import optimize_acqf
 from torch import Tensor
 
-from partial_kgfn.models.decoupled_gp_network import GaussianProcessNetwork
+from botorch.acquisition.fixed_feature import FixedFeatureAcquisitionFunction
+from fast_pkgfn.models.decoupled_gp_network import GaussianProcessNetwork
+from botorch.optim.optimize import optimize_acqf
 
 
 def optimize_discrete_acqf_for_function_network(
@@ -42,19 +41,26 @@ def optimize_discrete_acqf_for_function_network(
         num_restarts: The number of starting points for multistart acquisition
             function optimization.
         raw_samples: The number of samples for initialization.
-        parent_bounds: Tensor of lower and upper bounds for the outputs of each node.
+        parent_bounds: Optional[List[Tensor]]: List of tensors indicating bounds of parent nodes' outputs 
+            for each node. This will be used by p-KGFN when upstream-downstream assumption is not imposed.
         return_best_only: If False, outputs the solutions corresponding to all
             random restart initializations of the optimization.
-        impose_assump: If true, we impose the upstream-downstream restriction on the problem being considered.
+        return full tree: Whether to return the full tree consisting of `X_actual`
+            and `X_fantasies` for a OneShotAcquisitionFunction
+        impose_assump: A boolean variable indicating if the problem being considered has the 
+            upstream-downstream assumption imposed.
+
     Returns:
-        A tensor of suggested candidate and its correponding acquisition function value.
+        TODO
 
     """
     if impose_assump:
+        # We put [0] in node_idx[0] because we assume that the nodes in the node_group take the same input
         node_idx = (acq_function.X_evaluation_mask == 1).nonzero()
         parent_nodes = acq_function.model.dag.get_parent_nodes(node_idx[0])
         active_indices = acq_function.model.active_input_indices[node_idx[0]]
         bounds = bounds[:, active_indices]
+        # Case 1: root node
         if len(parent_nodes) == 0:
             candidates, acq_vals = optimize_acqf(
                 acq_function=acq_function,
@@ -62,20 +68,21 @@ def optimize_discrete_acqf_for_function_network(
                 q=1,
                 num_restarts=num_restarts,
                 raw_samples=raw_samples,
+                return_full_tree=False,
                 return_best_only=return_best_only,
                 options={
                     "batch_limit": 1,
                     "init_batch_limit": raw_samples,
-                },
+                },  # **kwargs,
             )
             return candidates, acq_vals.item()
-
+        # Case 2: non-root nodes
         num_active_inputs = len(acq_function.model.active_input_indices[node_idx[0]])
         node_inputs = retrieve_available_evaluations(
             acq_function.model, node_idx[0]
         ).to(
             torch.double
-        ) 
+        )  # We pass only the representative (singleton) of nodes in the node group.
         if node_inputs is None:
             return None, -torch.inf
         column_fixed = node_inputs.shape[-1]
@@ -92,14 +99,17 @@ def optimize_discrete_acqf_for_function_network(
                     .all(-1)
                     .any(-1)
                 ):
+                    # if node_inputs[i] in acq_function.model.train_X[node_idx]:
                     logger.info(
                         f"Node input {node_inputs[i]} has been evaluated. "
                         f"Skip KGFN optimization problem {i}!"
                     )
+                    # skip optimizing an acqf for fixing an input that has been evaluated
                     batched_candidates.append(node_inputs[i])
                     batched_acq_vals.append(None)
                     continue
                 else:
+                    # Optimize only X_fantasies with fixing X_actual. Use evaluate function instead.
                     acq_vals = acq_function.forward(node_inputs[i].unsqueeze(0))
             else:
                 acqf_fixed_node_inputs = FixedFeatureAcquisitionFunction(
@@ -122,6 +132,7 @@ def optimize_discrete_acqf_for_function_network(
                     raw_samples=raw_samples,
                     return_best_only=False,
                     options={"batch_limit": 1, "init_batch_limit": raw_samples},
+                    # **kwargs,
                 )
                 logger.info(
                     f"Node 1 problem {i} optimization takes {time.time() - t0:.4f} seconds"
@@ -148,9 +159,12 @@ def optimize_discrete_acqf_for_function_network(
         node_idx = (acq_function.X_evaluation_mask == 1).nonzero()
         parent_nodes = acq_function.model.dag.get_parent_nodes(node_idx[0])
         active_indices = acq_function.model.active_input_indices[node_idx[0]]
-        if parent_bounds[node_idx] is not None:
+        if parent_bounds[node_idx[0]] is not None:
             all_bounds = torch.cat(
-                (bounds[:, active_indices], parent_bounds[node_idx].to(torch.double)),
+                (
+                    bounds[:, active_indices],
+                    parent_bounds[node_idx[0]].to(torch.double),
+                ),
                 dim=-1,
             ).to(torch.double)
         else:
@@ -166,11 +180,12 @@ def optimize_discrete_acqf_for_function_network(
             options={
                 "batch_limit": 1,
                 "init_batch_limit": raw_samples,
-            },
+            },  # **kwargs,
         )
         return candidates, acq_vals.item()
 
     if return_best_only:
+        # exclude None's
         valid_indices = [
             i for i, acq_vals in enumerate(batched_acq_vals) if acq_vals is not None
         ]
@@ -179,6 +194,7 @@ def optimize_discrete_acqf_for_function_network(
 
         batched_acq_vals = [batched_acq_vals[i] for i in valid_indices]
         batched_candidates = [batched_candidates[i] for i in valid_indices]
+        # max across restarts
         arg_max_idx = [torch.argmax(acq_vals) for acq_vals in batched_acq_vals]
         batched_acq_vals = [
             acq_vals[idx] for acq_vals, idx in zip(batched_acq_vals, arg_max_idx)
@@ -187,6 +203,7 @@ def optimize_discrete_acqf_for_function_network(
             candidates[idx] for candidates, idx in zip(batched_candidates, arg_max_idx)
         ]
 
+        # max across node inputs
         arg_max_idx = np.argmax(
             [acq_val.detach().numpy() for acq_val in batched_acq_vals]
         )
@@ -214,7 +231,7 @@ def retrieve_available_evaluations(
         product of the number of available evaluations for each of the parent nodes of
         node `node_idx`, and `d` is the number of parent nodes of node `node_idx`.
     """
-    parent_nodes = model.dag.get_parent_nodes(node_idx)
+    parent_nodes = model.dag.get_parent_nodes(node_idx)  # node_idx is a singleton
     Ys = []
     for group in model.node_groups:
         if set(group).issubset(set(parent_nodes)):
@@ -226,6 +243,7 @@ def retrieve_available_evaluations(
 
     if len(Ys) == 1:
         return Y_group
+        # return torch.Tensor(Ys[0])
 
     res = []
     for element in itertools.product(*Ys):
